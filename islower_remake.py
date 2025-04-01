18962e5f4c5e463079175102635f482ea2b0cb22
aintyour-keii <klynepascual.edu@gmail.com>
2025-04-01 20:25:04 +0800

    islower code completion

--- islower_remake.py
+++ islower_remake.py
@@ -1,5 +1,13 @@
 # Ask the user to input a string
+user_input = input("Enter a string: ")
 # Initialize a boolean to track if the string is in all lower case
+is_lower = True
 # Itterate through the string
-# And check if the character is in range of "A-Z" then set the boolean to false and break
+for char in user_input:
+    # And check if the character is in range of "A-Z" then set the boolean to false and break
+    if 'A' <= char <= 'Z':
+        is_lower = False
+        break
+
 # Print the value of the boolean
+print(is_lower)
