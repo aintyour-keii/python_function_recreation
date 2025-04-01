c7ca8d0cb99b6a615c9713688feaf70ed73c6a03
aintyour-keii <klynepascual.edu@gmail.com>
2025-04-01 19:27:25 +0800

    removeprefix code completion

--- removeprefix_remake.py
+++ removeprefix_remake.py
@@ -1,5 +1,11 @@
 # Ask for the user input
+text_input = input("Enter a string: ")
 # Ask for the prefix to be removed
+prefix_input = input("Enter the prefix to be removed: ")
 # Check if the string starts with the prefix
-# If it does, remove the prefix and print the result
-# If it doesn't, print the string unmodified
+if text_input[len(prefix_input):] == prefix_input:
+    # If it does, remove the prefix and print the result
+    print(text_input[len(prefix_input):])
+else:
+    # If it doesn't, print the string unmodified
+    print(text_input)
