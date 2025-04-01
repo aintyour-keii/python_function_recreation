e5ff53f5d5389bc6e1520d9b2a88f428107055fb
aintyour-keii <klynepascual.edu@gmail.com>
2025-04-01 19:29:15 +0800

    lower code completion

--- lower_remake.py
+++ lower_remake.py
@@ -1,4 +1,8 @@
 # Ask the user for input a string
+user_input = input("Enter a string: ")
 # Create a translation table for uppercase to lowercase
+upper_to_lower = str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')
 # Convert the input to lowercase using translate
+lowered_input = user_input.translate(upper_to_lower)
 # Print the input
+print(lowered_input)
