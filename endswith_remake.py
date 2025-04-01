0419c9696d6a28fb437d71f2b659bb0fc89c5aa1
aintyour-keii <klynepascual.edu@gmail.com>
2025-04-01 19:31:37 +0800

    endswith code completion

--- endswith_remake.py
+++ endswith_remake.py
@@ -1,4 +1,8 @@
 # Ask for the user input
+user_input = input("Enter a string: ")
 # Ask for the suffix to be checked
+suffix_to_check = input("Enter a suffix to check: ")
 # Check if the suffix is at the end of the input
+endswith = user_input[-len(suffix_to_check):] == suffix_to_check
 # If it is, print True. Otherwise, print False
+print(endswith)
