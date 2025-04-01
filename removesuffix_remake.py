2b5615eefcef56e169e9f04a110117774acea74e
aintyour-keii <klynepascual.edu@gmail.com>
2025-04-01 20:21:55 +0800

    removesuffix code completion

--- removesuffix_remake.py
+++ removesuffix_remake.py
@@ -1,5 +1,11 @@
 # Ask for the user input
+user_input = input("Enter a string: ")
 # Ask for the suffix to be removed
+suffix_to_remove = input("Enter the suffix to be removed: ")
 # Check if the string ends with the suffix
-# If it does, remove the suffix and print the resultt
-# If it doesn't, print the string unmodified
+if user_input[-len(suffix_to_remove):] == suffix_to_remove:
+    # If it does, remove the suffix and print the result
+    print(user_input[:-len(suffix_to_remove)])
+else:
+    # If it doesn't, print the string unmodified
+    print(user_input)
