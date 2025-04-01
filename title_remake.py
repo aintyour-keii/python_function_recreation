8f2e91b0693019e88eac79997883f7bfabf00a1c
aintyour-keii <klynepascual.edu@gmail.com>
2025-04-01 19:22:48 +0800

    pseudocodes created

---
+++ title_remake.py
@@ -0,0 +1,7 @@
+# Ask the user to input a string
+# Split the string
+# Create a variable to store the modified string
+# Loop throught the parts of the string
+# Check if the modified string is still empty, then add the first word after using the '.capitalize()' on it
+# if the modified string is not empty, Use '.capitalize()' on each part and add it onto the modified string after a space
+# After looping, print the modified string variable
