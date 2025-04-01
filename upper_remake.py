4a6f140fb93c73d1be2a833ad161718d59e3a689
aintyour-keii <klynepascual.edu@gmail.com>
2025-04-01 20:23:14 +0800

    upper code completion

--- upper_remake.py
+++ upper_remake.py
@@ -1,4 +1,8 @@
 # Ask the user for input a string
+user_input = input("Enter a string: ")
 # Create a translation table for lowercase to uppercase
+lower_to_upper = str.maketrans('abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 # Convert the input to uppercase using translate
+uppercase_input = user_input.translate(lower_to_upper)
 # Print the input
+print(uppercase_input)
