641b9cc10b203c4384e295786cc9e6c87b9a99a6
aintyour-keii <klynepascual.edu@gmail.com>
2025-04-01 20:26:48 +0800

    startswith code completion

--- startswith_remake.py
+++ startswith_remake.py
@@ -1,4 +1,8 @@
 # Ask for the user input
+user_input = input("Enter a string: ")
 # Ask for the prefix to be checked
+prefix = input("Enter a prefix to check: ")
 # Check if the prefix is at the start of the input
+startswith =  user_input[:len(prefix)] == prefix
 # If it is, print True. Otherwise, print False
+print(startswith)
